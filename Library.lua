94c355a77386c1145c342bf7159c38b37ecc8326
getoush-sys <getoush-sys@users.noreply.github.com>
2026-09-19 20:43:31 -0300

    Tune slider track brightness

--- Library.lua
+++ Library.lua
@@ -7334,7 +7334,7 @@ do
             BackgroundColor3 = function()
                 return Slider.Disabled and Library.Scheme.OutlineColor or Library.Scheme.AccentColor
             end,
-            BackgroundTransparency = 0.2,
+            BackgroundTransparency = 0.4,
             Position = UDim2.fromScale(0, 1),
             Size = UDim2.new(1, 0, 0, 15),
             Text = "",
